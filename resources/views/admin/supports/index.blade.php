5c831ab3e190d8a7e90ecb41db37b950a4a17909
Luiz Fernando Marques <luiz.souza@cslr-wga.com.br>
2024-01-05 16:45:52 -0300

    add controllers and migrations

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,26 @@
+<h1>Listagem dos dúvidas</h1>
+
+<a href="{{ route ('supports.create') }}">Criar dúvida</a>
+
+
+
+<table>
+   <thead>
+      <th>assunto</th>
+      <th>status</th>
+      <th>descrição</th>
+      <th></th>
+   </thead>
+   <tbody>
+      @foreach($supports as $support)
+         <tr>
+            <td>{{ $support->subject }}</td>
+            <td>{{ $support->status }}</td>
+            <td>{{ $support->body }}</td>
+            <td> > </td>
+
+         </tr>
+
+      @endforeach
+   </tbody>
+</table>
