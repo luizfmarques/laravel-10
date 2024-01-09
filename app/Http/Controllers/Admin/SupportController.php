8c0307627eb5aab493fb3e0c09a09f6237d618fd
Luiz Fernando Marques <luiz.souza@cslr-wga.com.br>
2024-01-09 16:48:59 -0300

    included validation

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
 use Illuminate\Http\Request;
 use SplSubject;
@@ -33,9 +34,9 @@ class SupportController extends Controller
         return view('/admin/supports/create');
     }
 
-    public function store(Request $request, Support $support)
+    public function store(StoreUpdateSupport $request, Support $support)
     {
-        $data = $request->all();
+        $data = $request->validated();
         $data['status'] = 'a';
 
         $support->create($data);
@@ -53,7 +54,7 @@ class SupportController extends Controller
         return view('admin/supports.edit', compact('support'));
     }
 
-    public function update(Request $request, Support $support, string $id)
+    public function update(StoreUpdateSupport $request, Support $support, string $id)
     {
         if (!$support = $support->find($id)){
             return back();
@@ -63,7 +64,7 @@ class SupportController extends Controller
         // $support->body = $request->body;
         // $support->save();
 
-        $support->update($request->only(['subject', 'body']));
+        $support->update($request->validated());
         return redirect()->route('supports.index');
     }
 
