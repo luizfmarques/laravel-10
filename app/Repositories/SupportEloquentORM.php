befdf7cdba06f8f4ff61f0fba21f9a65f2282f2e
Luiz Fernando Marques <luiz.souza@cslr-wga.com.br>
2024-01-19 16:35:27 -0300

    updated paginate

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -25,7 +25,7 @@ class SupportEloquentORM implements SupportRepositoryInterface
             }
          })
          ->paginate($totalPerPage, ['*'], 'page', $page);
-      dd($result->toArray());
+      return new PaginationPresenter($result);
    }
 
    public function getAll(string $filter = null): array
